0f2a3963001b96078b3324630a5293d3e7bf76db
Gustavo Cavalcante <nowayhecodes@gmail.com>
2021-02-27 20:06:40 -0300

    inteface refactor

--- src/Escriba/Constants.cs
+++ src/Escriba/Constants.cs
@@ -1,7 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Escriba
 {
-    public class Constants
+    public static class Constants
     {
-        
+        static string DEFAULT_LOG_FILE_NAME = "escriba.log";
+
+        public enum LoggerModes
+        {
+            Console,
+            Custom,
+            File,
+            Off,
+        }
+
+        public enum LogColor
+        {
+            GREEN,
+            MAGENTA,
+            YELLOW,
+            RED,
+        }
+
+        public enum LogPrefix
+        {
+            INFO,
+            IMPORTANT,
+            WARNING,
+            ERROR,
+        }
+
+        public static Array LoggerModesOptions = new object[]
+        {
+            LoggerModes.Console,
+            LoggerModes.Custom,
+            LoggerModes.File,
+            LoggerModes.Off
+        };
     }
 }
--- src/Escriba/Interfaces.cs
+++ src/Escriba/Interfaces.cs
@@ -1,30 +1,9 @@
+using Escriba;
+
 namespace Escriba
 {
     public interface ICustomLogger
     {
-        void send(String prefix, object content);
-    }
-
-    public interface ILogType
-    {
-        string color(Color color);
-
-        void prefix(Prefix prefix);
-    }
-
-    private enum Color
-    {
-        GREEN = "green",
-        MAGENTA = "magenta",
-        YELLOW = "yellow",
-        RED = "red"
-    }
-
-    private enum Prefix
-    {
-        INFO,
-        IMPORTANT,
-        WARNING,
-        ERROR,
+        void send(string prefix, object content);
     }
 }
